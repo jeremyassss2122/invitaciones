8710e306efaaf8f108f85435e4ba9e2c7e4b74f6
BANCOPPEL\jjgutierrez <jjgutierrez@bancoppel.com>
2025-07-09 12:04:23 -0600

    AGREGAR BASE

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   adapter: vercel(),
 });
